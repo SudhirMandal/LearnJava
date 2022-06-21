3b9997ad42aed6bd85562cced848619a92b76f36
Sudhir <r.sudhirmandal@gmail.com>
2022-06-21 08:44:04 +0530

    FirstCommit

---
+++ HospitalDriver/src/main/java/com/sudhir/HospitalDriver/HospitalDriverApplication.java
@@ -0,0 +1,13 @@
+package com.sudhir.HospitalDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalDriverApplication.class, args);
+	}
+
+}
---
+++ HospitalDriver/src/test/java/com/sudhir/HospitalDriver/HospitalDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhir.HospitalDriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
